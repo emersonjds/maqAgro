0586ea058285cd734c7e3f1130bb70f233753065
Emerson Silva <emerson_jdss@hotmail.com>
2023-03-18 01:14:55 -0300

    adjust categories and default Texts

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,10 +1,9 @@
 import React, { useState } from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { Avatar, Divider, Searchbar, Text } from "react-native-paper";
+import { Box } from "../../components/Spacing";
 
-import Constants from "expo-constants";
-import { Avatar, Searchbar } from "react-native-paper";
-
-// import { Container } from './styles';
+import * as S from "./styles";
+import { categoriesData } from "./category";
 
 const Home: React.FC = () => {
   const [searchQuery, setSearchQuery] = useState("");
@@ -13,14 +12,10 @@ const Home: React.FC = () => {
   const onChangeText = (query) => setSearchQuery(query);
 
   return (
-    <View style={styles.container}>
+    <S.Container>
       <Avatar.Image size={50} source={require("../../assets/img/avatar.png")} />
 
-      <View
-        style={{
-          marginTop: 20,
-        }}
-      >
+      <Box mt={20}>
         <Searchbar
           placeholder="Pesquise por algo para sua propriedade"
           numberOfLines={1}
@@ -28,48 +23,28 @@ const Home: React.FC = () => {
           onChangeText={(text) => onChangeText(text)}
           value={text}
         />
-      </View>
+      </Box>
 
-      <View
-        style={{
-          flexDirection: "row",
-          justifyContent: "space-between",
-          width: "90%",
-          marginLeft: 20,
-        }}
-      >
-        <View
-          style={{
-            alignItems: "center",
-            marginTop: 20,
-            justifyContent: "center",
-          }}
-        >
-          <Avatar.Image size={90} source={require("../../assets/img/1.jpeg")} />
-          <Text
-            style={{
-              marginTop: 10,
-            }}
-          >
-            Veiculos
-          </Text>
-        </View>
-      </View>
-    </View>
+      <Box mt={20} flexDirection="row" justifyContent="space-between">
+        {categoriesData.map((item) => (
+          <Box justifyContent="center" alignItems="center">
+            <Avatar.Image size={90} source={item.image} />
+            <Text
+              style={{
+                marginTop: 10,
+              }}
+            >
+              {item.name}
+            </Text>
+          </Box>
+        ))}
+      </Box>
+
+      <Box mt={20}>
+        <Divider />
+      </Box>
+    </S.Container>
   );
 };
 
 export default Home;
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    // justifyContent: 'center',
-    paddingTop: Constants.statusBarHeight,
-    // backgroundColor: '#ecf0f1',
-    padding: 8,
-  },
-  searchbar: {
-    width: "100%",
-  },
-});
