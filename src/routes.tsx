c0b9b84ee121a4ac25d6fb9c13d03a117498d925
Emerson Silva <emerson_jdss@hotmail.com>
2023-03-18 03:13:07 -0300

    add and config react-native-chart-kit

--- src/routes.tsx
+++ src/routes.tsx
@@ -7,6 +7,7 @@ import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { AntDesign } from "@expo/vector-icons";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import Inputs from "./screens/Inputs";
+import HomeScreen from "./screens/Home";
 
 const Tab = createBottomTabNavigator();
 const Stack = createNativeStackNavigator();
@@ -15,10 +16,24 @@ const MyTabs = () => {
   return (
     <Tab.Navigator>
       <Tab.Screen
-        name="Home"
-        component={Home}
+        name="HomeScreen"
+        component={HomeScreen}
         options={{
           tabBarLabel: "Home",
+          tabBarLabelStyle: {
+            color: "#000",
+          },
+          headerShown: false,
+          tabBarIcon: ({ color }) => (
+            <AntDesign name="home" size={24} color="black" />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="HomeScreen"
+        component={HomeScreen}
+        options={{
+          tabBarLabel: "Meus Pedidos",
           tabBarLabelStyle: {
             color: "#000",
           },
