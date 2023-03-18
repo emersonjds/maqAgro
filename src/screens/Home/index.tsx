11acc24dad42164b125a5151212e8e79ea5ce99a
Emerson Silva <emerson_jdss@hotmail.com>
2023-03-17 23:38:05 -0300

    adjust import image

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,10 +1,38 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { useState } from "react";
+import { StyleSheet, View } from "react-native";
+
+import Constants from "expo-constants";
+import { Avatar } from "react-native-paper";
 
 // import { Container } from './styles';
 
 const Home: React.FC = () => {
-  return <View />;
-}
+  const [searchQuery, setSearchQuery] = useState("");
+  const [text, setText] = useState("");
+
+  const onChangeText = (query) => setSearchQuery(query);
+
+  return (
+    <View style={styles.container}>
+      <Avatar.Image
+        size={50}
+        source={require("../../assets/img/avatar.png")}
+      />
+    </View>
+  );
+};
 
 export default Home;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    // justifyContent: 'center',
+    paddingTop: Constants.statusBarHeight,
+    // backgroundColor: '#ecf0f1',
+    padding: 8,
+  },
+  searchbar: {
+    width: "100%",
+  },
+});
