abdd57b503bf759265d8aa4fb1a5183f75d34040
Emerson Silva <emerson_jdss@hotmail.com>
2023-03-18 03:12:49 -0300

    add new route

--- src/screens/Inputs/index.tsx
+++ src/screens/Inputs/index.tsx
@@ -1,6 +1,6 @@
-import React from "react";
-import { FlatList, StyleSheet, View } from "react-native";
-import { Card, Title } from "react-native-paper";
+import React, { FC } from "react";
+import { FlatList, Image, ScrollView, StyleSheet, View } from "react-native";
+import { Card, Divider, Text, Title } from "react-native-paper";
 
 import { Box } from "../../components/Spacing";
 
@@ -23,25 +23,108 @@ const agribusinessInputs = [
   // Add more agribusiness input items as needed
 ];
 
-const Inputs: React.FC = () => {
-  const renderItem = ({ item }) => (
-    <Card style={styles.card}>
-      <Card.Cover source={{ uri: item.image }} />
-      <Card.Content>
-        <Title>{item.title}</Title>
-      </Card.Content>
-    </Card>
-  );
+const Inputs: FC = () => /* const renderItem = ({ item }) => (*/ /*   <Card style={styles.card}>*/ /*     <Card.Cover source={{ uri: item.image }} />*/ /*     <Card.Content>*/ /*       <Title>{item.title}</Title>*/ /*     </Card.Content>*/ /*   </Card>*/ /* );*/(
+  // <FlatList
+  //   data={agribusinessInputs}
+  //   renderItem={renderItem}
+  //   keyExtractor={(item) => item.id}
+  //   contentContainerStyle={styles.listContainer}
+  // />
+  <View
+    styles={{
+      flex: 1,
+    }}
+  >
+    <Box
+      ml={20}
+      mt={20}
+    >
+      <Title>Sugeridos para voce</Title>
+      <Box
+        mt={10}
+        mb={10}
+      >
+        <Divider />
+      </Box>
 
-  return (
-    <FlatList
-      data={agribusinessInputs}
-      renderItem={renderItem}
-      keyExtractor={(item) => item.id}
-      contentContainerStyle={styles.listContainer}
-    />
-  );
-};
+    </Box>
+    <ScrollView horizontal showsHorizontalScrollIndicator={false}
+      contentContainerStyle={{
+        justifyContent: "space-between",
+        width: "100%",
+        alignItems: "center",
+        paddingHorizontal: 10,
+      }}
+    >
+      <Box
+        alignItems="center"
+        justifyContent="center"
+        borderRadius={10}
+        borderWidth={1}
+        width={120}
+        height={170}
+        padding={10}
+      >
+        <Image
+          source={require("../../assets/img/3.webp")}
+          style={{ width: 100, height: 100 }}
+        />
+        <Text>
+          100l Nitrato {'\n'} de Potassio
+          {'\n'} Fracionado
+        </Text>
+      </Box>
+
+      <Box
+        alignItems="center"
+        justifyContent="center"
+        borderRadius={10}
+        borderWidth={1}
+        width={120}
+        height={170}
+        padding={10}
+      >
+        <Image
+          source={require("../../assets/img/3.webp")}
+          style={{ width: 100, height: 100 }}
+        />
+        <Text>
+          100l Nitrato {'\n'} de Potassio
+          {'\n'} Fracionado
+        </Text>
+      </Box>
+
+      <Box
+        alignItems="center"
+        justifyContent="center"
+        borderRadius={10}
+        borderWidth={1}
+        width={120}
+        height={170}
+        padding={10}
+      >
+        <Image
+          source={require("../../assets/img/3.webp")}
+          style={{ width: 100, height: 100 }}
+        />
+        <Text>
+          100l Nitrato {'\n'} de Potassio
+          {'\n'} Fracionado
+        </Text>
+      </Box>
+
+
+    </ScrollView>
+    <Box
+      mt={10}
+      mb={10}
+    >
+      <Divider />
+    </Box>
+
+  </View>
+
+);
 
 export default Inputs;
 
